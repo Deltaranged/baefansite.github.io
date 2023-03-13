6165ba1d24acb61f492f900e5a894fe10a021928
Delta Ranged <delta.ranged@gmail.com>
2023-03-14 01:43:11 +1100

    Use static adapter for svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +10,18 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: dev ? '' : '/baefansite.github.io',
+		}
 	}
 };
 
